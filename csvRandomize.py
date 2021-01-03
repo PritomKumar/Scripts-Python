d4511186f8a928fde88f20def83bf20095084747
PritomKumar <bsse0919@iit.du.ac.bd>
2021-01-03 22:11:20 +0600

    Line splitting complete

--- csvRandomize.py
+++ csvRandomize.py
@@ -1,14 +1,19 @@
-import csv
+f = open("sheet1.csv", "r")
 
+# print(f.read())
 
-with open('sheet1.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        print(row)
+lines = f.readlines()
 
-        if line_count == 0:
-            #print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+count = 0
+for line in lines: 
+    count +=1
+    # print("Line{}: {}".format(count, line.strip())) 
+    line = line.strip()
+    if(line == "More than 32"):
+        line = "33-50"
+    splittedLine  = line.split('-')
+    print(splittedLine)
+
+file1 = open('myfile.csv', 'w') 
+file1.writelines("ffsdf") 
+file1.close() 
