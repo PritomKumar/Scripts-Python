c1853d061f54a2b4978e123c2a23d672dfdc5eb0
PritomKumar <bsse0919@iit.du.ac.bd>
2021-01-03 22:23:28 +0600

    Script working

--- csvRandomize.py
+++ csvRandomize.py
@@ -1,8 +1,10 @@
+import random
 f = open("sheet1.csv", "r")
 
 # print(f.read())
 
 lines = f.readlines()
+file1 = open('myfile.csv', 'w') 
 
 count = 0
 for line in lines: 
@@ -12,8 +14,11 @@ for line in lines:
     if(line == "More than 32"):
         line = "33-50"
     splittedLine  = line.split('-')
-    print(splittedLine)
+    # print(splittedLine)
+    randomInt = random.randint(int(splittedLine[0]),int(splittedLine[1]))
+    # print(randomInt)
+    file1.write(str(randomInt) + "\n") 
 
-file1 = open('myfile.csv', 'w') 
-file1.writelines("ffsdf") 
+
+
 file1.close() 
